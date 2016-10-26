d4e8343dbd08a7fd59061eb5d193d688af0857dd
Martín Cortina <hellraiserii@gmail.com>
2016-10-26 19:11:53 -0300

    Initial commit

---
+++ app/src/main/java/ar/com/telecom/guarda/LockScreenActivity.java
@@ -0,0 +1,14 @@
+package ar.com.telecom.guarda;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LockScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+
+}
---
+++ app/src/main/java/ar/com/telecom/guarda/MainActivity.java
@@ -0,0 +1,68 @@
+package ar.com.telecom.guarda;
+
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String TAG ="PRUEBAAA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+        Toast t = Toast.makeText(getApplicationContext(), "Hola Marce", Toast.LENGTH_LONG);
+        t.show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+
+    }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+
+
+
+}
